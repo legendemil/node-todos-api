c0db5b366946bed181a96c1a6d62e45d813c1f49
legendemil <emilpausz@gmail.com>
2017-06-22 16:30:00 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
 	todo.save().then(doc => {
 		res.send(doc);
 	}).catch(err => {
-		res.status(400).send(e);
+		res.status(400).send(err);
 	});
 });
 
@@ -25,3 +25,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = {
+	app
+}
